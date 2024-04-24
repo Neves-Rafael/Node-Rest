6f81ceb64dbd81be2b0c8b4f62b4231eb080a931
Rafael Neves <nevesrafael.dev@gmail.com>
2024-04-24 18:09:43 -0300

    :tada: Commit init

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from "fastify";
+import { knex } from "./database";
+
+const server = fastify()
+
+server.get("/hello", async() => {
+  const test = await knex("sqlite_schema")
+  return test  
+})
+
+server.listen({
+  port: 3333,
+}).then(() => {
+  console.log("HTTP Server running 🔥🔥🔥")
+})
