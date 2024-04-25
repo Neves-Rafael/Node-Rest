f12b5d2b91e2f69bc77cb40717d3d1ca6cdcb399
Rafael Neves <nevesrafael.dev@gmail.com>
2024-04-25 20:35:55 -0300

    :sparkles: Feat transaction route

---
+++ src/routes/transactions.ts
@@ -0,0 +1,25 @@
+import { FastifyInstance } from "fastify";
+import { knex } from "../database";
+import { randomUUID } from "node:crypto";
+import { z } from "zod";
+
+export async function transactionRoute(server: FastifyInstance){
+  server.post("/", async (request, reply) => {
+
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"])
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(request.body)
+
+    await knex("transactions").insert({
+      id: randomUUID(),
+      title,
+      amount: type === "credit" ? amount : amount * -1,
+    })
+    
+    return reply.status(201).send()
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,12 @@
 import fastify from "fastify";
-import { knex } from "./database";
-import { randomUUID } from "node:crypto";
 import { env } from "./env";
+import { transactionRoute } from "./routes/transactions";
 
-const server = fastify()
+const server = fastify();
 
-server.get("/hello", async(req, res) => {
-  const transaction = await knex("transactions").insert({
-    id: randomUUID(),
-    title: "Test create",
-    amount: 1000
-  }).returning("*")
-
-  return transaction 
-})
+server.register(transactionRoute,{
+  prefix: "transactions"
+});
 
 server.listen({
   port: env.PORT,
