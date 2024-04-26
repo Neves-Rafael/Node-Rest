6819c92b8c27a471517dc034c6886c1db05e1903
Rafael Neves <nevesrafael.dev@gmail.com>
2024-04-25 22:20:54 -0300

    :sparkles: Feat get function in transaction route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,26 @@ import { randomUUID } from "node:crypto";
 import { z } from "zod";
 
 export async function transactionRoute(server: FastifyInstance){
+  server.get("/", async (request, reply) =>{
+    const transactions = await knex("transactions").select()
+
+    return reply.send({transactions})
+  })
+
+  server.get("/:id", async (request, reply) =>{
+    const idParams = z.object({
+      id: z.string().uuid(),
+    })
+    
+    const { id } = idParams.parse(request.params)
+    
+    const transactions = await knex("transactions").where({id}).first()
+    
+    console.log(transactions)
+    return reply.send({transactions})
+  })
+
+
   server.post("/", async (request, reply) => {
 
     const createTransactionBodySchema = z.object({
