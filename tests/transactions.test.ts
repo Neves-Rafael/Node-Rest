48bda729b4edd0428944d6fd23d90d3f004d2dcb
Rafael Neves <nevesrafael.dev@gmail.com>
2024-04-27 22:52:53 -0300

    :sparkles: Feat env.test

--- tests/transactions.test.ts
+++ tests/transactions.test.ts
@@ -1,6 +1,7 @@
-import { test, beforeAll, afterAll, describe, expect } from "vitest";
+import { test, beforeAll, afterAll, describe, expect, beforeEach } from "vitest";
 import { app } from "../src/app";
 import supertest from "supertest";
+import { execSync } from "node:child_process"
 
 describe("Transactions Routes", ()=> {
   beforeAll(()=>{
@@ -10,6 +11,12 @@ describe("Transactions Routes", ()=> {
   afterAll(() => {
     app.close()
   })
+
+  beforeEach(()=>{
+    execSync("npm run knex migrate:rollback --all")
+    execSync("npm run knex migrate:latest")
+  })
+
   
   test("User can create a new transaction", async () => {
     await supertest(app.server)
@@ -33,19 +40,19 @@ describe("Transactions Routes", ()=> {
         amount: 1000,
         type: "credit"
       })
-
-
+      
+    console.log(createTransactionResponse.body)
+    
     const cookies = createTransactionResponse.get("Set-Cookie")
     
     if(!cookies){
-      console.log(cookies)
       throw new Error("cookie not found")
     }
-
+    
     const listTransactionResponse = await supertest(app.server)
-      .get("/transactions")
-      .set("Cookie", cookies)
-      .expect(200)
+    .get("/transactions")
+    .set("Cookie", cookies)
+    .expect(200)
     
     expect(listTransactionResponse.body.transactions).toEqual([
       expect.objectContaining({
