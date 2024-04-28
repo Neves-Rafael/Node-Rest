27042e2b902a046cba6f7e5edd67a67818c9715d
Rafael Neves <nevesrafael.dev@gmail.com>
2024-04-28 19:28:18 -0300

    :sparkles: Feat get and summary transictions test

--- tests/transactions.test.ts
+++ tests/transactions.test.ts
@@ -41,8 +41,6 @@ describe("Transactions Routes", ()=> {
         type: "credit"
       })
       
-    console.log(createTransactionResponse.body)
-    
     const cookies = createTransactionResponse.get("Set-Cookie")
     
     if(!cookies){
@@ -60,6 +58,74 @@ describe("Transactions Routes", ()=> {
         amount: 1000,
       })
     ])
-    
   })
+
+  test("List a specific transaction", async () => {
+    const createTransactionResponse = await supertest(app.server)
+      .post("/transactions")
+      .send({
+        title: "New transaction",
+        amount: 1000,
+        type: "credit"
+      })
+      
+    const cookies = createTransactionResponse.get("Set-Cookie")
+    
+    if(!cookies){
+      throw new Error("cookie not found")
+    }
+    
+    const listTransactionResponse = await supertest(app.server)
+    .get("/transactions")
+    .set("Cookie", cookies)
+    .expect(200)
+
+    const transactionId = listTransactionResponse.body.transactions[0].id
+
+    const getTransactionResponse = await supertest(app.server)
+    .get(`/transactions/${transactionId}`)
+    .set("Cookie", cookies)
+    .expect(200)
+    
+    expect(getTransactionResponse.body.transactions).toEqual(
+      expect.objectContaining({
+        title: "New transaction",
+        amount: 1000,
+      })
+    )
+  })
+
+  test("Get the summary", async () => {
+    const createTransactionResponse = await supertest(app.server)
+      .post("/transactions")
+      .send({
+        title: "Credit transaction",
+        amount: 1000,
+        type: "credit"
+      })
+          
+    const cookies = createTransactionResponse.get("Set-Cookie")
+
+    if(!cookies){
+      throw new Error("cookie not found")
+    }
+
+    await supertest(app.server)
+      .post("/transactions")
+      .set("Cookie", cookies)
+      .send({
+        title: "Debit transaction",
+        amount: 500,
+        type: "debit"
+      })
+    
+    const SummaryResponse = await supertest(app.server)
+    .get("/transactions/summary")
+    .set("Cookie", cookies)
+    .expect(200)
+    
+    expect(SummaryResponse.body.summary).toEqual({
+      amount: 500,
+    })
+  })  
 })
