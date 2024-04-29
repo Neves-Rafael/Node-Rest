cdb66be85e64beb1bbd39394c230d77caefc744c
Rafael Neves <nevesrafael.dev@gmail.com>
2024-04-28 22:04:03 -0300

    :bug: Fix add host server

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { env } from "./env"
 
 app.listen({
   port: env.PORT,
+  host: 'RENDER' in process.env ? `0.0.0.0` : `localhost`,
 }).then(() => {
   console.log("HTTP Server running 🔥🔥🔥")
 })
